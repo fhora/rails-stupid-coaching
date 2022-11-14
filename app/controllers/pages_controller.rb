502dbc3b3bb64937f5573b08a15c6c5f164a32bd
fabiola hora <fabiola.hordinola@gmail.com>
2022-11-14 14:32:50 +0100

    stupid coaching partie1 ok

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    @answer = ''
+    if @question == 'I am going to work'
+      return @answer = 'Great!'
+    elsif @question.include?('?')
+      return @answer = 'Silly question, get dressed and go to work!'
+    else
+      return @answer = 'I don\'t care, get dressed and go to work!'
+    end
+  end
+end
